d2c5a7f61873cedb5265b7ff46a112cd1b5bd08b
sabeerahameds <69470338+sabeerahameds@users.noreply.github.com>
2022-12-07 11:03:50 +0530

    Update sabeer_day_7.py

--- 2022/sabeer_day_7.py
+++ 2022/sabeer_day_7.py
@@ -1,7 +1,1 @@
-# marker_position = 4
-with open("/home/sabeer-ss/Downloads/input-day7-sample.txt", "r") as f:
-    input_list = f.readlines()
-    for idx, input_str in enumerate(input_list):
-        input_str = input_str.strip()
-        if input_str.startswith()
-        print(input_str)
+  
