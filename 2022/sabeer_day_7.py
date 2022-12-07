20f23a02fca02d6116b1e7cd4e977eeaee759399
sabeerahameds <69470338+sabeerahameds@users.noreply.github.com>
2022-12-07 11:02:12 +0530

    Add files via upload

---
+++ 2022/sabeer_day_7.py
@@ -0,0 +1,7 @@
+# marker_position = 4
+with open("/home/sabeer-ss/Downloads/input-day7-sample.txt", "r") as f:
+    input_list = f.readlines()
+    for idx, input_str in enumerate(input_list):
+        input_str = input_str.strip()
+        if input_str.startswith()
+        print(input_str)
